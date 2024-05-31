79f3a6107e3701652adfa1cbecd3f625b11854b4
sookiemonster <sookiemonster216@gmail.com>
2024-05-30 20:05:37 -0400

    Add rough foods.google scraping routine.

--- FindGF/scrape.js
+++ FindGF/scrape.js
@@ -1,35 +1,57 @@
-// import puppeteer from 'puppeteer';
+// Import puppeteer web scraper module
 var puppeteer = require('puppeteer');
 
-const foo = async() => {
+// Redefine timeout
+const TIMEOUT_MS = 10000; 
+
+// Selectors for menu-items
+const orderOnlineSelector = 'a[href^="https://food.google.com/chooseprovider"';
+const menuItemSelector = '.WV4Bob';
+
+const scrapeMap = async() => {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   // Navigate the page to a URL
-  await page.goto('https://developer.chrome.com/');
+  await page.goto('https://maps.google.com/?cid=17299868738663185922');
 
   // Set screen size
   await page.setViewport({width: 1080, height: 1024});
 
-  // Type into search box
-  await page.type('.devsite-search-field', 'automate beyond recorder');
+  // Navigating to Google Food
+  try {
+    // Wait for Order Online Button to appear
+    await page.waitForSelector(orderOnlineSelector, {timeout: TIMEOUT_MS});
+  } catch (error) {
+    // The Order Online Button did not appear (loading failure or order online not available)
+    return [];
+  }
 
-  // Wait and click on first result
-  const searchResultSelector = '.devsite-result-item-link';
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
 
-  // Locate the full title with a unique string
-  const textSelector = await page.waitForSelector(
-    'text/Customize the Recorder'
-  );
-  const fullTitle = await textSelector?.evaluate(el => el.textContent);
+  await page.click(orderOnlineSelector);
 
-  // Print the full title
-  console.log('The title of this blog post is "%s".', fullTitle);
+  // Scrape available menu items
+  let menuItems = [];
+  try {
+    // Wait for menu items to appear
+    await page.waitForSelector(menuItemSelector, {timeout: TIMEOUT_MS});
+    // Take the text content from menu items (name, price, description) and propogate allItems
+    menuItems = await page.evaluate(() => 
+      [...document.querySelectorAll('.WV4Bob')].map(({ innerText }) => innerText)
+    );
+  } catch (error) {
+    // Search other vendors
+  }
 
   await browser.close();
+  return menuItems;
 };
 
-foo();
+scrapeMap()
+  .then((items) => {
+    items.forEach(element => {
+      console.log(element);
+    });
+  });
+  // .catch(TimeoutError)
