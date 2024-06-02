537d2ebbe8fa7490431d0b4205eb82060508f6ff
sookiemonster <sookiemonster216@gmail.com>
2024-06-02 19:29:36 -0400

    Create default react app

---
+++ FindGF/routes/api.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.post('/find-nearby', function(req, res) {
+
+   res.send();
+});
+
+module.exports = router;
