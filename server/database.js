4a861cb4e5caa067271b59d8c9101e40502bbd41
sookiemonster <sookiemonster216@gmail.com>
2024-06-12 09:14:43 -0400

    Add database wrapper

--- server/database.js
+++ server/database.js
@@ -5,6 +5,17 @@ import 'dotenv/config';
 
 import { isError, voidExceptID } from './gf-codes.js';
 
+var Database = function() {
+   // Initialize Database manager
+   this.pool = new Pool({ ssl: true });
+   
+   // Query methods
+   this.updateRestaurantDetails = updateRestaurantDetails.bind(this);
+
+   // Close pool
+   this.end = () => { this.pool.end(); }
+}
+
 // id = text
 // name = text
 // lat = num 8,6
@@ -16,55 +27,39 @@ import { isError, voidExceptID } from './gf-codes.js';
 // reviews = json[]
 // items = json[]
 
-let updateRestaurantDetails = async(pool, resJSON) => {
+async function updateRestaurantDetails(resJSON) {
    // Do not propogate details if error. Only store ID.
    if (isError(resJSON)) { resJSON = voidExceptID(resJSON); }
 
    // do updates instead of inserting if already in table:
+   // https://stackoverflow.com/questions/1109061/insert-on-duplicate-update-in-postgresql/1109198#1109198
 
    const query = {
       text: 'INSERT INTO places (id, name, lat, long, mapuri, summary, gfrank, reviews, items) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)',
       values: [resJSON.id, resJSON.name, resJSON.lat, resJSON.long, resJSON.mapUri, resJSON.gfSum, resJSON.gfRank, resJSON.gfReviews, resJSON.gfItems]
    };
-   https://stackoverflow.com/questions/1109061/insert-on-duplicate-update-in-postgresql/1109198#1109198
-   return pool.query(query);
+   return this.pool.query(query);
 }
 
-let pool = new Pool({
-   ssl: true
-});
+let db = new Database;
 
-// await updateRestaurantDetails(pool, 
-//    {
-//       "id" : "test4",
-//       "name" : "test",
-//       "lat" : 23.442,
-//       "long": 180.233,
-//       "mapUri" : "test.com",
-//       "gfSum" : "A summary!",
-//       "gfRank" : 3, 
-//       "gfReviews" : [{"author" : "sarah", "text" : "a review!"}],
-//       "gfItems" : [{"item" : "borgar", "description" : "very tasty."}],
-//       "resolveAttempts" : 0
-//    }
-// )
+db.updateRestaurantDetails(
+   {
+      "id" : "test4",
+      "name" : "test",
+      "lat" : 23.442,
+      "long": 180.233,
+      "mapUri" : "test.com",
+      "gfSum" : "A summary!",
+      "gfRank" : 3, 
+      "gfReviews" : [{"author" : "sarah", "text" : "a review!"}],
+      "gfItems" : [{"item" : "borgar", "description" : "very tasty."}],
+      "resolveAttempts" : 0
+   }
+)
 
-// await updateRestaurantDetails(pool, 
-//    {
-//       "id" : "test5",
-//       "name" : "test",
-//       "lat" : 23.442,
-//       "long": 180.233,
-//       "mapUri" : "test.com",
-//       "gfSum" : "A summary!",
-//       "gfRank" : -1, 
-//       "gfReviews" : [{"author" : "sarah", "text" : "a review!"}],
-//       "gfItems" : [{"item" : "borgar", "description" : "very tasty."}],
-//       "resolveAttempts" : 0
-//    }
-// )
 
-pool.query("SELECT * FROM places;")
-   .then((result) => console.log(result.rows));
+// pool.query("SELECT * FROM places;")
+//    .then((result) => console.log(result.rows));
    
-pool.end();
+db.end();
