f24cd4362b87e515322507ea448e0e51a640a479
sookiemonster <sookiemonster216@gmail.com>
2024-06-13 11:31:33 -0400

    Add isValidSearch() to test if a search query should be performed. Add push log to add records of previous search queries

--- server/database.js
+++ server/database.js
@@ -4,6 +4,9 @@ const { Pool } = pg;
 import 'dotenv/config'; 
 
 import { isError, voidExceptID, needsReview } from './gf-codes.js';
+const LATITUDE_TOLERANCE = 0.001; // .001 degrees ~= 100m
+const LONGITUDE_TOLERANCE = 0.001;
+
 
 var Database = function() {
    // Initialize Database manager
@@ -11,6 +14,8 @@ var Database = function() {
    
    // Query methods
    this.updateRestaurantDetails = updateRestaurantDetails.bind(this);
+   this.pushLog = pushLog.bind(this);
+   this.isValidSearch = isValidSearch.bind(this);
    this.getRestaurant = getRestaurant.bind(this);
    this.getAllInBounds = getAllInBounds.bind(this);
 
@@ -71,5 +76,49 @@ async function updateRestaurantDetails(resJSON) {
    };
    return this.pool.query(query);
 }
+
+async function pushLog(lat, long) {
+   if (!lat || !long) { return; }
+   
+   // Store logs with 4 decimal places (since highly unlikely for very close coordinates to be the same)
+   lat = lat.toFixed(4);
+   long = long.toFixed(4);
+   // Create a unique string token by appending latitudes and longitudes
+   const query = {
+      text: `INSERT INTO log (id, lat, long) 
+               VALUES($1, $2, $3) 
+               ON CONFLICT(id) 
+               DO UPDATE SET last_updated = NOW()::DATE`,
+      values: [lat.toString() + long.toString(), lat, long]
+   }
+   return this.pool.query(query);
+}
+
+async function isValidSearch(point) {
+   if (!point) { return; }
+
+   // 20 days for a search nearby request to be stale; get all logs that are near the specified point
+   const query = {
+      text: `SELECT id FROM log WHERE last_updated >= NOW()::DATE - INTERVAL '20 days' AND lat BETWEEN $1 AND $2 AND long BETWEEN $3 AND $4`,
+      values: [point.lat - LATITUDE_TOLERANCE, point.lat + LATITUDE_TOLERANCE, point.long - LONGITUDE_TOLERANCE, point.long + LONGITUDE_TOLERANCE]
+   }
+
+   return new Promise((resolve, reject) => {
+      this.pool.query(query)
+         .then((res) => {
+            console.log(res);
+            return resolve(res.rows.length == 0);
+         })
+         .catch((err) => {
+            console.log(err)
+            return reject(true);
+         });
+   });
+}
+
+let test = new Database();
+test.isValidSearch({ lat: 0.0010, long: 23.4})
+   .then(res => console.log(res))
+   .catch(err => console.error(err));
 
 export { Database };
