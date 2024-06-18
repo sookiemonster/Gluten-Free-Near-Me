84ad7548e93c90ca36ec4cae2e781ee302dc9383
sookiemonster <sookiemonster216@gmail.com>
2024-06-18 01:00:12 -0400

    Rename ReduxHandler to Store

--- client/src/App.js
+++ client/src/App.js
@@ -2,34 +2,34 @@ import logo from './logo.svg';
 import React from 'react';
 // import { findNearby } from './script.js';
 import Restaurants from './Restaurants.js';
-import { store } from './ReduxHandler.js';
+import store from './Store.js';
 import './master.css';
 // import './master.css';
 
 function App() {
   // Append map file
   return (
-    <div className="App">
-      <div id="sidebar">
-        <h1>Gluten Free Near Me</h1>
-        <button id="finder">Send request</button>
+      <div className="App">
+        <div id="sidebar">
+          <h1>Gluten Free Near Me</h1>
+          <button id="finder">Send request</button>
 
-        <Restaurants />
-        <div id="search-container">
-          <span id="search-prompt">Search </span>
+          <Restaurants />
+          <div id="search-container">
+            <span id="search-prompt">Search </span>
 
-          </div>
-          <nav id="nav">
-              <a className="active" href="/">Home</a>
-              <a href="/about">About</a>
-              <a href="">Documentation</a>
-          </nav>
+            </div>
+            <nav id="nav">
+                <a className="active" href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="">Documentation</a>
+            </nav>
+        </div>
+
+        <div id="map-container">
+          <div id="map"></div>
+        </div>
       </div>
-
-      <div id="map-container">
-        <div id="map"></div>
-      </div>
-    </div>
   );
 }
 
--- client/src/Store.js
+++ client/src/Store.js
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import resReducer, { resAdded } from './RestaurantSlice.js'
 
-export const store = configureStore({
-  reducer: {
-    restaurants: resReducer,
-  }
-})
+var store = configureStore({
+    reducer: {
+      restaurants: resReducer,
+    }
+  });
+
 
 console.log(store.getState().restaurants);
 store.dispatch(resAdded(20))
 console.log(store.getState().restaurants);
+
+export default store;
