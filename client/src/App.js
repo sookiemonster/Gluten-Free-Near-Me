fe7c86a94e98f9ef76caeb9b0244adfd81744adc
sookiemonster <sookiemonster216@gmail.com>
2024-06-18 22:52:34 -0400

    Move socket.io handling to client app from external script

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import React from 'react';
+import React, { useEffect, useState }from 'react';
 // import { findNearby } from './script.js';
 import Restaurants from './Restaurants.js';
 import store from './Store.js';
@@ -7,9 +7,35 @@ import MapContainer from './MapContainer.js';
 import './master.css';
 import { Provider } from 'react-redux'
 import { APIProvider } from '@vis.gl/react-google-maps';
+import io from 'socket.io-client';
+import { resAdded } from './RestaurantSlice.js';
 // import { resAdded } from './RestaurantSlice.js';
 
+
 function App() {
+  const socket = io("wss://localhost:5000", { transports : ['websocket'] });
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    function onConnect() {
+      console.log('connected');
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onRestaurantFound(resJSON) {
+      store.dispatch(resAdded(resJSON));
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('restaurant', onRestaurantFound);
+  }, [socket]);
+
+
   // Append map file
   return (
     <Provider store={store}>
