7a19580f415e99d4db4f19cc77a67795c2135a40
sookiemonster <sookiemonster216@gmail.com>
2024-06-18 22:27:27 -0400

    Clean-up comments & stray tests

--- client/src/MapContainer.js
+++ client/src/MapContainer.js
@@ -1,61 +1,11 @@
 import {AdvancedMarker, Map, Marker, Pin } from '@vis.gl/react-google-maps';
-import React, { useEffect } from 'react'; 
-import { useSelector, useDispatch } from 'react-redux'
-import { resAdded } from './RestaurantSlice';
+import React from 'react'; 
+import { useSelector } from 'react-redux'
 
 function MapContainer() {
    let restaurants = useSelector((state) => state.restaurants.resList);
    console.log("state in component: ", restaurants);
-   // const dispatch = useDispatch();
-
-   // function addMarker() {
-   //    const obj = {
-   //       "name" : "addwd",
-   //       id : "awdwd",
-   //       lat : 40.7174,
-   //       long : -73.985,
-   //    };
-   //    console.log('uh');
-   //    setMarkerColleciton([...markerCollection, obj])
-   // }
-
-   console.log(<div id="map-container">
-      <Map
-      id="map"
-      style={{width: '100%', height: '100%'}}
-      defaultCenter={{lat: 40.7174, lng: -73.985}}
-      defaultZoom={18}
-      minZoom={17}
-      maxZoom={20}
-      disableDefaultUI={true}
-      mapId={"beb67932d5e5a0ce"}>
-         { restaurants.map((place, index) => 
-            <Marker 
-               key = {place.id}
-               label={{ text:"awooga!",
-                  color:'red', 
-                  fontWeight: "500", 
-                  className: "pin-title", 
-                  path: ""}} 
-               position={{lat: place.lat, lng: place.lng}} 
-            />
-         )}
-         <div id="something"></div>
-         {/* { restaurants.map((place) => console.log(place)) } */}
-         {/* <AdvancedMarker position={{lat: 40.7174, lng: -73.985}} />
-         <AdvancedMarker position={{lat: 40.7174, lng: -73.985}}>
-            <Pin
-               background={'#0f9d58'}
-               borderColor={'#006425'}
-               glyphColor={'#60d98f'}
-            />
-         </AdvancedMarker> */}
-         {/* { markerCollection.map(markerData => (<Marker key={markerData.id} position={{lat: 40.7174, lng: -73.985}} />))} */}
-         {/* { markerCollection.map(markerData => { console.log(markerData); } )} */}
-      </Map>
-   </div>)
    return (
-      // <APIProvider apiKey={process.env.REACT_APP_API_KEY}>
       <div id="map-container">
          <Map
          id="map"
@@ -77,17 +27,6 @@ function MapContainer() {
                   position={{lat: Number(place.lat), lng: Number(place.lng)}} 
                />
             )}
-            {/* { restaurants.map((place) => console.log(place)) } */}
-            {/* <AdvancedMarker position={{lat: 40.7174, lng: -73.985}} />
-            <AdvancedMarker position={{lat: 40.7174, lng: -73.985}}>
-               <Pin
-                  background={'#0f9d58'}
-                  borderColor={'#006425'}
-                  glyphColor={'#60d98f'}
-               />
-            </AdvancedMarker> */}
-            {/* { markerCollection.map(markerData => (<Marker key={markerData.id} position={{lat: 40.7174, lng: -73.985}} />))} */}
-            {/* { markerCollection.map(markerData => { console.log(markerData); } )} */}
          </Map>
       </div>
    );
--- client/src/Store.js
+++ client/src/Store.js
@@ -1,38 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
-import resReducer, { resAdded } from './RestaurantSlice.js'
+import resReducer from './RestaurantSlice.js'
 
 var store = configureStore({
-    reducer: {
-      restaurants: resReducer,
-    }
-  });
-
-
-console.log(store.getState().restaurants);
-store.dispatch(resAdded({
-  id: "aa",
-  name: "test1!",
-  lat: 40.7174, 
-  lng: -73.985
-}))
-store.dispatch(resAdded({
-  id: "aba",
-  name: "test2",
-  lat: 40.717, 
-  lng: -73.985
-}))
-store.dispatch(resAdded({
-  id: "aca",
-  name: "test3",
-  lat: 40.7168, 
-  lng: -73.985
-}))
-store.dispatch(resAdded({
-  id: "a3a",
-  name: "test4",
-  lat: 40.7169, 
-  lng: -73.985
-}))
-console.log(store.getState().restaurants);
+  reducer: {
+    restaurants: resReducer,
+  }
+});
 
 export default store;
