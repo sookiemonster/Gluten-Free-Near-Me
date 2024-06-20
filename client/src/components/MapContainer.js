1fb4b0948dfe078327a49af741f030bd679f529a
sookiemonster <sookiemonster216@gmail.com>
2024-06-20 09:09:24 -0400

    Add custom marker icon. Add scroll to restaurant in sidebar upon clicking the marker.

--- client/src/components/MapContainer.js
+++ client/src/components/MapContainer.js
@@ -3,33 +3,51 @@ import React from 'react';
 import { useSelector } from 'react-redux'
 
 // Define marker color-scheme
-const markerScheme = {
+const markerStrokeScheme = {
    "3" : "#123811",
    "2" : "#903B0B",
    "1" : "#421102"
 }
 
-const tagScheme = {
-   "3" : "green",
-   "2" : "yellow",
-   "1" : "brown"
+const markerFillScheme = {
+   "3" : "#25A305",
+   "2" : "#F6B409",
+   "1" : "#631700"
 }
 
 function GFMarker({place}) {
-   const markerColor = markerScheme[String(place.gfrank)];
-   const tagColorClass = tagScheme[String(place.gfrank)];
+   const markerStroke = markerStrokeScheme[String(place.gfrank)];
+   const markerFill = markerFillScheme[String(place.gfrank)];
+
+   const scrollToCard = () => {
+      const restaurantCard = document.querySelector( '#card-' + place.id );
+      restaurantCard.scrollIntoView( { behavior: 'smooth', block: 'start' } );
+   }
+
    return (<Marker 
       key = {place.id}
       label={{ text: place.name,
-         color: markerColor , 
+         color: markerFill , 
          fontWeight: "500", 
-         className: "pin-title " + tagColorClass, 
+         className: "pin-title", 
          path: ""}} 
-      position={{lat: Number(place.lat), lng: Number(place.long)
-      }} 
+      position={{lat: Number(place.lat), lng: Number(place.long) }} 
+      icon= {
+         {
+            path: "M22.8,11.2c0.1,4.3-5.9,10-8,13.6c-2.4,4-2.4,4-3.4,7.7c-0.7-3.8-0.7-3.8-3.1-7.7C6.2,21.2,0,15.5,0,11.4C0,3.8,7.5-2.1,15.5,0.7C19.9,2.2,22.7,6.5,22.8,11.2z",
+            fillColor: markerFill,
+            fillOpacity: 1.0,
+            strokeColor: markerStroke,
+            strokeOpacity: 0.25,
+            labelOrigin: {x: 16, y: 30}
+         }
+      }
+      onClick={scrollToCard}
    />
    );
 }
+
+
 
 function MapContainer() {
    let restaurants = useSelector((state) => state.restaurants.resList);
