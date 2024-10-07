6dc32daf036490247b861d63865406a5281ddbd1
Daniel Sooknanan <daniel.sooknanan216@gmail.com>
2024-10-07 17:02:19 -0400

    It works! But only w/out headless.

--- server/scrape.js
+++ server/scrape.js
@@ -8,10 +8,12 @@ import { Cluster } from 'puppeteer-cluster';
 import { RestaurantDetails } from './gf-codes.js';
 
 // Selectors for elements
-const orderOnlineSelector = 'a[href^="https://food.google.com/chooseprovider"';
-const itemContainerSelector = '.WV4Bob';
-const itemNameSelector = '.bWZFsc';
-const itemDescSelector = '.gQjSre';
+const orderOnlineSelector = 'a[href^="https://www.google.com/viewer/chooseprovider'
+const providerSelector = 'div[ssk="23:2Storefront by DoorDash"] > a'
+
+const menuItemSelector = 'div[data-anchor-id="MenuItem"]'
+const nameSelector = 'h3[data-telemetry-id="storeMenuItem.title"]'
+const descSelector = 'span[data-telemetry-id="storeMenuItem.subtitle"]'
 
 // Redefine timeout & configure browser options
 export const TIMEOUT_MS = 5000; 
@@ -19,70 +21,83 @@ export const TIMEOUT_MS = 5000;
 const cluster = await Cluster.launch({
   concurrency: Cluster.CONCURRENCY_CONTEXT,
   maxConcurrency: 5,
+  puppeteerOptions: {
+    headless: false}
 })
 
 await cluster.task(async({ page, data }) => {
-  console.log(`ATTEMPTING SCRAPE: ${data}`)
   const mapuri = data;
+  console.log(`ATTEMPTING SCRAPE: ${data}`)
+
   page.setDefaultTimeout(TIMEOUT_MS);
   page.setDefaultNavigationTimeout(TIMEOUT_MS);
-
-  try {
-    await page.goto(mapuri);
-  } catch (error) {
-    throw {name: "LINK_NOT_ACCESSIBLE", message: "There was a problem accessing the URL: " + mapUri}; 
-  }
-  
-  // Set screen size
   await page.setViewport({width: 1080, height: 1024});
 
   // Disable image loading
   await page.setRequestInterception(true);
   page.on('request', (req) => {
-    if(req.resourceType() === 'image' || req.resourceType() === 'media' || req.resourceType() === 'stylesheet' || req.resourceType() === 'font') { req.abort();}
+    if(req.resourceType() === 'image') { req.abort();}
     else { req.continue(); }
   });
 
-  let mapstart = Date.now();
   try {
-    await page.waitForNavigation({waitUntil: 'domcontentloaded'});
-    console.log("Took", Date.now() - mapstart, " ms to load the Google Maps DOM", mapuri);
-    await page.waitForSelector(orderOnlineSelector);
-    console.log("Took", Date.now() - mapstart, " ms to load the Google Maps Page", mapuri);
+      await page.goto(mapuri);
+      await page.waitForNavigation({waitUntil: 'domcontentloaded'});
   } catch (error) {
-    // Order Online button doesn't appear
-    console.log("Took", Date.now() - mapstart, " ms. But errored on loading the Google Order Online Selector (on Maps)", mapuri);
-    throw {name: "MENU_NOT_FOUND", message: "Could not access menu given the URL: " + mapuri};
+      throw {name: "LINK_NOT_ACCESSIBLE", message: "There was a problem accessing the URL: " + mapUri}; 
   }
-  await page.click(orderOnlineSelector);
-  await page.waitForNavigation({waitUntil: 'domcontentloaded'});
-  let foodStart = Date.now();
+    
   try {
-    await page.waitForSelector(itemContainerSelector);
-    console.log("Took", Date.now() - foodStart, " ms to load the Google Foods Page", mapuri);
-  } catch (error) {
-    // Google Foods menu doesn't appear (only lists third-party providers)
-    console.log("Took", Date.now() - foodStart, " ms to have trouble accessing the menu on the Google Foods Page", mapuri);
-    throw {name: "MENU_NOT_FOUND", message: "Could not access menu given the URL: " + mapuri};
+    // Get the Order Online Button
+    await page.waitForSelector(orderOnlineSelector);
+    await page.click(orderOnlineSelector);
+    await page.waitForNavigation({waitUntil: 'domcontentloaded'});
+    
+    // Navigate to DoorDash StoreFront 
+    await page.waitForSelector(providerSelector);
+    await page.click(providerSelector);
+    await page.waitForNavigation({waitUntil: 'domcontentloaded'});
+
+  } catch (error) {   
+    throw {name: "MENU_NOT_FOUND", message: "There was a problem accessing the URL: " + mapUri}; 
   }
 
   console.log("MADE IT TO MENU_ITEMS")
 
-  const menuItems = await page.evaluate((itemContainerSelector, itemNameSelector, itemDescSelector) => {
-    // Get all menu item containers
-    const productCollection = document.querySelectorAll(itemContainerSelector);
-    const result = [];
-    
-    // Get the name and description for each menu item.
-    productCollection.forEach((product) => {
-      const itemName = product.querySelector(itemNameSelector)?.innerText;
-      const itemDescription = product.querySelector(itemDescSelector)?.innerText;
-
-      result.push({name : itemName, desc: itemDescription});
+  const menuItems = await page.evaluate(async (menuItemSelector, nameSelector, descSelector) => {
+    return new Promise((resolve, reject) => {
+      const distance = 140 * 3;
+      var heightObject = { totalHeight: 0};
+      var result = [];
+      var unique = new Set();
+        
+      const timer = setInterval(() => {
+          if (heightObject.totalHeight >= document.body.scrollHeight) {
+              clearInterval(timer);
+              console.log("complte!", result);
+              resolve(result);
+          } else {
+              window.scrollBy(0, distance);
+              heightObject.totalHeight += distance;
+              
+              console.log("selecting items: ", menuItemSelector);
+              const productCollection = document.querySelectorAll(menuItemSelector);
+              
+              console.log("queried:", productCollection);
+              productCollection.forEach((product) => {
+                  const itemName = product.querySelector(nameSelector)?.innerText;
+                  
+                  if (!unique.has(itemName)) {
+                      // Get the description and add if we haven't already
+                      const itemDescription = product.querySelector(descSelector)?.innerText;
+                      unique.add(itemName);
+                      result.push({name : itemName, desc: itemDescription});
+                  }
+              })
+          }
+      }, 250);
     })
-
-    return result;
-  }, itemContainerSelector, itemNameSelector, itemDescSelector);
+  }, menuItemSelector, nameSelector, descSelector )
 
   return menuItems;
 });
--- server/test.js
+++ server/test.js
@@ -1,12 +1,13 @@
 import puppeteer from 'puppeteer';
+
+import { filterGFMenuItems } from './parse-gluten-free.js';
+
 // Or import puppeteer from 'puppeteer-core';
 
 // Launch the browser and open a new blank page
 const browser = await puppeteer.launch({headless: false});
 const page = await browser.newPage();
 
-// DEMO
-const mapuri = "https://maps.app.goo.gl/bi1eU7xkQYeq1xZb8"
 
 const orderOnlineSelector = 'a[href^="https://www.google.com/viewer/chooseprovider'
 const providerSelector = 'div[ssk="23:2Storefront by DoorDash"] > a'
@@ -16,43 +17,45 @@ const menuItemSelector = 'div[data-anchor-id="MenuItem"]'
 const nameSelector = 'h3[data-telemetry-id="storeMenuItem.title"]'
 const descSelector = 'span[data-telemetry-id="storeMenuItem.subtitle"]'
 
-await page.setViewport({width: 1080, height: 1080});
-// Navigate the page to a URL.
+// DEMO
+// const mapuri = "https://maps.app.goo.gl/2Dued4dsnuWnf8Ni8"
+// await page.setViewport({width: 1080, height: 1080});
+// // Navigate the page to a URL.
 
-await page.setRequestInterception(true);
-page.on('request', (req) => {
-  if(req.resourceType() === 'image') { req.abort();}
-  else { req.continue(); }
-});
+// await page.setRequestInterception(true);
+// page.on('request', (req) => {
+//   if(req.resourceType() === 'image') { req.abort();}
+//   else { req.continue(); }
+// });
 
-if (false){
-    try {
-        await page.goto(mapuri);
-    } catch (error) {
-        throw {name: "LINK_NOT_ACCESSIBLE", message: "There was a problem accessing the URL: " + mapUri}; 
-    }
-    
-    await page.waitForNavigation({waitUntil: 'domcontentloaded'});
-    // console.log("Took", Date.now() - mapstart, " ms to load the Google Maps DOM", mapuri);
-    await page.waitForSelector(orderOnlineSelector);
-    await page.click(orderOnlineSelector);
-    await page.waitForNavigation({waitUntil: 'domcontentloaded'});
-    
-    // console.log("Took", Date.now() - mapstart, " ms to load the Google Maps DOM", mapuri);
-    await page.waitForSelector(providerSelector);
-    await page.click(providerSelector);
-    await page.waitForNavigation({waitUntil: 'domcontentloaded'});
-    
-}
 
-await page.goto("https://order.online/store/neirs-tavern-woodhaven-732524/?hideModal=true&pickup=true&rwg_token=AJKvS9UWLLRzKA97VepY72JweA-ssHpzy7lS_TSwqZMGNknyjcRFqMZnVQF5sgAq-rKchHS7Wv1uJy6mYkAxi0n1F3DJ2Qw8vA==&utm_source=gfo");
+// try {
+//     await page.goto(mapuri);
+// } catch (error) {
+//     throw {name: "LINK_NOT_ACCESSIBLE", message: "There was a problem accessing the URL: " + mapUri}; 
+// }
+
+// await page.waitForNavigation({waitUntil: 'domcontentloaded'});
+// // console.log("Took", Date.now() - mapstart, " ms to load the Google Maps DOM", mapuri);
+// await page.waitForSelector(orderOnlineSelector);
+// await page.click(orderOnlineSelector);
+// await page.waitForNavigation({waitUntil: 'domcontentloaded'});
+
+// // console.log("Took", Date.now() - mapstart, " ms to load the Google Maps DOM", mapuri);
+// await page.waitForSelector(providerSelector);
+// await page.click(providerSelector);
+// await page.waitForNavigation({waitUntil: 'domcontentloaded'});
+    
+
+await page.goto("https://order.online/store/kotti-berliner-d%C3%B6ner-kebab-new-york-1362365/?hideModal=true&pickup=true&rwg_token=AJKvS9VQ9cjqlfSEkiGK-hwygnC8V3qdwboPXFWhm-xOihuJcwdu3ecHH3vwSLFzt_aINfemlVoMSV9eI3iWRhiOetNJc0A8AA==&utm_source=gfo");
 await page.waitForNavigation({waitUntil: 'domcontentloaded'});
 
 const menuItems = await page.evaluate(async (menuItemSelector, nameSelector, descSelector) => {
     return new Promise((resolve, reject) => {
-        const distance = 140 * 4.5;
+        const distance = 140 * 3;
         var heightObject = { totalHeight: 0};
         var result = [];
+        var unique = new Set();
         
         const timer = setInterval(() => {
             if (heightObject.totalHeight >= document.body.scrollHeight) {
@@ -69,16 +72,21 @@ const menuItems = await page.evaluate(async (menuItemSelector, nameSelector, des
                 console.log("queried:", productCollection);
                 productCollection.forEach((product) => {
                     const itemName = product.querySelector(nameSelector)?.innerText;
-                    const itemDescription = product.querySelector(descSelector)?.innerText;                
-                    result.push({name : itemName, desc: itemDescription});
+                    
+                    if (!unique.has(itemName)) {
+                        // Get the description and add if we haven't already
+                        const itemDescription = product.querySelector(descSelector)?.innerText;
+                        unique.add(itemName);
+                        result.push({name : itemName, desc: itemDescription});
+                    }
                 })
             }
-        }, 100);
+        }, 250);
     })
 }, menuItemSelector, nameSelector, descSelector )
 
+filterGFMenuItems(menuItems);
 console.log(menuItems);
-
 // console.log(items);
 
 
